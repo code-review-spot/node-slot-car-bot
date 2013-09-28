cb174cf0bfb93ba8d574b6cc9f83b690138eb7c3
Matthew Chase Whittemore <matthew@socialtables.com>
2013-09-28 19:18:43 -0400

    adding user interface to example

--- examples/single-car-simple.js
+++ examples/single-car-simple.js
@@ -9,19 +9,33 @@ MySlotCarSet.on("ready", function(){
 
 	var Yoshi = MySlotCarSet.addCar({pin:6});
 
+	var updateDisplay = function(){
+		process.stdout.write('\u001B[2J\u001B[0;0f');
+		console.log("Car is", Yoshi.isOn ? "on" : "off");
+		console.log("Engine", Yoshi.isOn ? "are" : "will be", "at", (100/Yoshi.fidelity)*Yoshi.speedData.rate, "%");
+		console.log("Press SPACE to", Yoshi.isOn ? "START" : "STOP", "the car");
+		console.log("Press UP ARROW to increase speed");
+		console.log("Press DOWN ARROW to decrease speed");
+		console.log("Prese CTRL+C to quit");
+	}
+
+	Yoshi.on("speed-change", function(data){
+		updateDisplay();
+	});
+
 	stdin.on('keypress', function (chunk, key) {
 	  if (key && key.ctrl && key.name == 'c'){
 	  	Yoshi.pin.low();
 	  	process.exit();
 	  }
-	  else if(key && key.name == 's'){
+	  else if(key && key.name == 'down'){
 	  	var rate = Yoshi.speedData.rate - 30;
 	  	if(rate<0){
 	  		rate = 0;
 	  	}
 	  	Yoshi.setSpeed({rate:rate});
 	  }
-	  else if(key && key.name == 'w'){
+	  else if(key && key.name == 'up'){
 	  	var rate = Yoshi.speedData.rate + 30;
 	  	if(rate>Yoshi.fidelity){
 	  		rate = Yoshi.fidelity;
@@ -30,13 +44,18 @@ MySlotCarSet.on("ready", function(){
 	  }
 	  else if(key && key.name == 'space'){
 	  	if(Yoshi.isOn){
-	  		console.log("STOP THIS CAR");
 	  		Yoshi.stop();
+	  		updateDisplay();
 	  	}
 	  	else{
-	  		console.log("START THIS CAR");
 	  		Yoshi.start();
+	  		updateDisplay();
 	  	}
 	  }
+	  else{
+	  	updateDisplay();
+	  }
 	});
+
+	updateDisplay();
 });
